dbd810685cc85ff8968b96478b3979adc08b2eab
Maciejlys <maciej.lyszczarz98@gmail.com>
2022-01-12 23:09:56 +0100

    deployment

--- build/index.js
+++ build/index.js
@@ -21,6 +21,7 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv = __importStar(require("dotenv"));
 const discord_js_1 = require("discord.js");
+const http = __importStar(require("http"));
 dotenv.config();
 const token = process.env.DISCORD_BOT_TOKEN;
 const roles = {
@@ -35,57 +36,64 @@ const roles = {
     Paladin: "Paladin",
     DK: "DK",
 };
-const client = new discord_js_1.Client({
-    intents: [discord_js_1.Intents.FLAGS.GUILDS, discord_js_1.Intents.FLAGS.GUILD_MESSAGES],
-});
-client.on("ready", () => {
-    console.log("Ready!");
-});
-client.on("messageCreate", async (message) => {
-    if (message.content.startsWith(`!inv`)) {
-        const eventId = message.content.split(" ")[1];
-        if (eventId) {
-            message.channel.messages
-                .fetch()
-                .then((e) => {
-                const targetMsg = e.find((a) => a.id === eventId);
-                const fields = targetMsg?.embeds.map((a) => a.fields);
-                const nicknames = [];
-                if (fields) {
-                    nicknames.push(`Macro to invite people from ${eventId}`);
-                    for (const field of fields) {
-                        const values = field.map((m) => m.value);
-                        for (const value of values) {
-                            for (const role in roles) {
-                                const role_name_regex = new RegExp("__(" + role + ")__", "gm");
-                                if (role_name_regex.test(value)) {
-                                    const pattern = /\*\*(.+)\*\*/;
-                                    const regex = new RegExp(pattern, "gm");
-                                    const result = value.match(regex);
-                                    if (result) {
-                                        result.shift();
-                                        result.forEach((f) => {
-                                            nicknames.push("/invite " + f.replace(/\*/g, ""));
-                                        });
+const app = () => {
+    const client = new discord_js_1.Client({
+        intents: [discord_js_1.Intents.FLAGS.GUILDS, discord_js_1.Intents.FLAGS.GUILD_MESSAGES],
+    });
+    client.on("ready", () => {
+        console.log("Ready!");
+    });
+    client.on("messageCreate", async (message) => {
+        if (message.content.startsWith(`!inv`)) {
+            const eventId = message.content.split(" ")[1];
+            if (eventId) {
+                message.channel.messages
+                    .fetch()
+                    .then((e) => {
+                    const targetMsg = e.find((a) => a.id === eventId);
+                    const fields = targetMsg?.embeds.map((a) => a.fields);
+                    const nicknames = [];
+                    if (fields) {
+                        nicknames.push(`Macro to invite people from ${eventId}`);
+                        for (const field of fields) {
+                            const values = field.map((m) => m.value);
+                            for (const value of values) {
+                                for (const role in roles) {
+                                    const role_name_regex = new RegExp("__(" + role + ")__", "gm");
+                                    if (role_name_regex.test(value)) {
+                                        const pattern = /\*\*(.+)\*\*/;
+                                        const regex = new RegExp(pattern, "gm");
+                                        const result = value.match(regex);
+                                        if (result) {
+                                            result.shift();
+                                            result.forEach((f) => {
+                                                nicknames.push("/invite " + f.replace(/\*/g, ""));
+                                            });
+                                        }
                                     }
                                 }
                             }
                         }
+                        const macro = nicknames.join("\n");
+                        message.author.send(macro);
                     }
-                    const macro = nicknames.join("\n");
-                    message.author.send(macro);
-                }
-            })
-                .catch((e) => {
-                console.log(e);
-                message.author.send("Error");
-            });
+                })
+                    .catch((e) => {
+                    console.log(e);
+                    message.author.send("Error");
+                });
+            }
+            else {
+                message.author.send("Error, provide an event id");
+            }
+            message.delete();
         }
-        else {
-            message.author.send("Error, provide an event id");
-        }
-        message.delete();
-    }
+    });
+    client.login(token);
+};
+var server = http.createServer((req, res) => { });
+server.listen(process.env.PORT || 80, () => {
+    console.log("Listening");
 });
-client.login(token);
+app();
 //# sourceMappingURL=index.js.map
